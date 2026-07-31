c7455f9be827b0c5551e9d7ed2c9fe500873e991
Tomáš Ivan <tomas.ivan.it@gmail.com>
2026-07-31 11:25:00 +0200

    Repository secrets

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import datetime
+import os
 
 now = datetime.datetime.now(datetime.timezone.utc)
-log_message = f"Automated run executed at {now.isoformat()}\n"
+token = os.environ.get("SEC_TOKEN", "No token found")
+log_message = f"Automated run at {now.isoformat()} with token: {token}\n"
 print(log_message.strip())
-
 with open("log.txt", "a", encoding="utf-8") as f:
     f.write(log_message)
